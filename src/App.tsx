5bd8debda04c780f53d1cad803ae71cb6a8a270c
YonatanToker <yonatantoker123@gmail.com>
2024-06-28 16:03:11 +0300

    with the input fields set and controlled components

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import "./App.css";
 import commaNumber from "comma-number";
 function App() {
   const [loanAmount, setLoanAmount] = useState("");
-  useEffect(() => {
-    console.log(loanAmount);
-  }, [loanAmount]);
+  const [apr, setApr] = useState(4.5);
+  const [years, setYears] = useState(15);
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const rawValue = e.target.value.replace(/,/g, "");
     if (!isNaN(Number(rawValue))) {
@@ -14,11 +13,43 @@ function App() {
   };
   return (
     <>
-      <input
-        type="text"
-        value={commaNumber(loanAmount)}
-        onChange={(e) => handleInputChange(e)}
-      />
+      <div className="container">
+        <div className="input-field-wrapper">
+          <label for="loan-amount">Loan amount</label>
+          <input
+            type="text"
+            value={commaNumber(loanAmount)}
+            onChange={(e) => handleInputChange(e)}
+            required
+            name="loan-amount"
+            id="loan-amount"
+          />
+          $
+        </div>
+        <div className="input-field-wrapper">
+          <label for="apr">Annual Interest Rate</label>
+          <input
+            type="number"
+            value={apr}
+            name="apr"
+            id="apr"
+            required
+            onChange={(e) => setApr(Number(e.target.value))}
+          />
+          %
+        </div>
+        <div className="input-field-wrapper">
+          <label for="years">Years Amount</label>
+          <input
+            type="number"
+            value={years}
+            name="years"
+            id="years"
+            required
+            onChange={(e) => setYears(Number(e.target.value))}
+          />
+        </div>
+      </div>
     </>
   );
 }
